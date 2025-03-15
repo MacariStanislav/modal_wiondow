8bded23b4a52060cc97cef8b8600328894fa5cc9
Macari Stanislav <stasmacari63@gmail.com>
2025-03-15 15:16:10 +0200

    ss

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import StoreProvider from "./storeProvider";
-
+// 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
